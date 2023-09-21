8ec40a9f08611e159db8f47e4f2fc8c7f079367f
Henry Efamba Jr <efambajr.henry@gmail.com>
2023-09-21 17:32:53 -0500

    starting off on the right foot

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Intro from './Intro';
+
+function App() {
+  return (
+    <>
+      <Intro />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Intro.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Container.css';
+
+export default function Intro() {
+  return (
+    <div className="container">
+      <h1>Intro</h1>
+    </div>
+  );
+}
